d7e44e33a9775743a761d8135eec5b55f424e260
Zohaib479 <don462671@gmail.com>
2025-07-30 02:48:00 +0500

    deploying via vercel

--- src/App.jsx
+++ src/App.jsx
@@ -33,13 +33,15 @@ const App = () => {
    <Navbar/>
    <AnimatedHeading/>
      <Routes>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/home' element={<Home/>} />
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='/gallery' element={<Gallery/>} />
-      <Route path='/events' element={<Events/>}/>
-      <Route path='/careers' element={<Careers/>}/>
-    </Routes>
+  <Route path='/' element={<Home />} />            {/* Default Route */}
+  <Route path='/home' element={<Home />} />
+  <Route path='/about' element={<About />} />
+  <Route path='/contact' element={<Contact />} />
+  <Route path='/gallery' element={<Gallery />} />
+  <Route path='/events' element={<Events />} />
+  <Route path='/careers' element={<Careers />} />
+</Routes>
+
     <Footer/>
    </>
     
